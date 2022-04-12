9c3c0a7e09f75108accb3d1d2c14f70d12f1271a
dimon <dimarru1@rambler.ru>
2022-04-13 01:07:07 +0300

    Save six

--- One.py
+++ One.py
@@ -4,9 +4,12 @@ import platform
 info = 'OS info is \n{} \n\nPython version is {} {}'.format(
     platform.uname(), sys.version, platform.architecture())
 print(info)
+info.isalpha()
 
 with open('os_info.txt', 'w') as ff:
     ff.write(info)
+
+
 
 print('Dimon')
 
